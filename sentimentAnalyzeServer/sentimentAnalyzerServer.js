e31641f85fb2906c7473d8d45cb015788b5ea94d
romanes13 <romanosmalamitsis@gmail.com>
2021-09-22 18:07:52 +0000

    changes to getNLUInstance

--- sentimentAnalyzeServer/sentimentAnalyzerServer.js
+++ sentimentAnalyzeServer/sentimentAnalyzerServer.js
@@ -19,10 +19,31 @@ variables that you set up in the .env file*/
 // const api_url = process.env.API_URL;
 
 function getNLUInstance() {
-    /*Type the code to create the NLU instance and return it.
-    You can refer to the image in the instructions document
-    to do the same.*/
-}
+    let api_key = process.env.API_KEY
+    let api_url = process.env.API_URL
+    const NaturalLanguageUnderstandingV1 = require('ibm-watson/natural-language-understanding/v1');
+    const { IamAuthenticator } = require('ibm-watson/auth');
+
+    const naturalLanguageUnderstanding = new NaturalLanguageUnderstandingV1({
+        version: '2021-08-01',
+        authenticator: new IamAuthenticator({
+            apikey: api_key,
+            }),
+        serviceUrl:api_url,
+        });
+    return naturalLanguageUnderstanding ;
+} 
+
+
+const analyzeParams = {
+    'url': 'www.ibm.com',
+    'features': {
+    'categories': {
+      'limit': 3
+    }
+  }
+};
+
 
 
 //The default endpoint for the webserver
